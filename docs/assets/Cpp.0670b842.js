95d29c3db83b53f61d08e3eccc654d7756ece9be
Nekilc <nieaowei@gmail.com>
2022-11-22 22:00:25 +0800

    deploy

---
+++ docs/assets/Cpp.0670b842.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./index.88b47ff4.js";const e={name:"Cpp"};function n(p,r,o,s,c,_){return null}const f=t(e,[["render",n]]);export{f as default};
--- src/views/Go.vue
+++ src/views/Go.vue
@@ -5,17 +5,21 @@ import RustCard from "../components/RustCard.vue";
 </script>
 
 <template>
-  <template v-for="item in rust">
-    <h3>{{ item.title }}</h3>
-    <el-row :gutter="8">
-      <el-col class="col" v-for="data in item.docs" :xs="12" :sm="8" :md="8" :lg="4" :xl="4">
-        <rust-card :data="data"></rust-card>
-      </el-col>
-    </el-row>
-  </template>
+  <div>
+    <template v-for="item in rust">
+      <h3>{{ item.title }}</h3>
+      <el-row :gutter="8">
+        <el-col class="col" v-for="data in item.docs" :xs="12" :sm="8" :md="8" :lg="4" :xl="4">
+          <rust-card :data="data"></rust-card>
+        </el-col>
+      </el-row>
+    </template>
+  </div>
 </template>
 
 
 <style scoped>
-
+.col {
+  margin-bottom: 0.5em;
+}
 </style>
